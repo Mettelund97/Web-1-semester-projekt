75565ad72bc8856e34ff2b9dcb1d244ad2334106
Mettelund3 <105641445+Mettelund3@users.noreply.github.com>
2024-10-03 12:09:17 +0200

    navbar

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("burger-menu").addEventListener("click", function() {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+  const burger = document.getElementById("burger-menu");
+  burger.classList.toggle("active");
+});
