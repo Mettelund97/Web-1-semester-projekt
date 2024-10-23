98635c796b428e071c5b16870b7d1a6e6613a599
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2024-10-23 10:44:13 +0200

    index routes correct

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
-
-const { engine } = require('express-handlebars');
-
-
+const exphbs = require("express-handlebars");
 const path = require("path");
+
 const PORT = 3000;
+
 app.use(express.json());
 
 
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+  })
+);
+app.set("view engine", "hbs");
 
 
-// Set up Handlebars as the template engine
-app.engine('hbs', engine({
-    defaultLayout: 'main',  
-    extname: '.hbs'
-}));
-
+// Middleware to handle JSON
+app.use(express.json());
 
 
 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,22 +1,7 @@
 
-const express = require("express");
-const app = express();
-
-const { engine } = require('express-handlebars');
-const path = require("path");
-app.set('view engine', 'hbs');
-
-
-
-// Set up Handlebars as the template engine
-app.engine('hbs', engine({
-    defaultLayout: 'main',  
-    extname: '.hbs'
-}));
-
-app.get('/', (reg, res) => {
-  res.render('index');
-});
+exports.getHome = (req, res) => {
+  res.render("index");
+};
 
 /*
 exports.getHome = (req, res) => {
