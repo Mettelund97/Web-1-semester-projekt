1bdae0570ad32f14ad0eced9a933f9dba1809c62
BawarHamid <bawar@live.dk>
2024-10-23 11:26:07 +0200

    unless conditional for login-page

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res) => {
   res.render("login", {
     title: "Login",
+    hideSidebar: true, //variable til at bestemme om nav/sidebar skal være synlig eller ej.
   });
 };
