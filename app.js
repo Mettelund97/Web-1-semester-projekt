9524c597efca7c1eee56be5a33f22bdcecac6f18
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2024-10-23 10:24:14 +0200

    1

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require("express");
 const app = express();
 
+const { engine } = require('express-handlebars');
+
+
 const path = require("path");
 const PORT = 3000;
 app.use(express.json());
 
+
+app.set('view engine', 'hbs');
+
+
+// Set up Handlebars as the template engine
+app.engine('hbs', engine({
+    defaultLayout: 'main',  
+    extname: '.hbs'
+}));
+
+
+
+
 // Static files middleware
 app.use(express.static(path.join(__dirname, "public")));
 
+
 const homeController = require("./controllers/homeController");
+/*
 const settingsController = require("./controllers/settingsController");
 const loginController = require("./controllers/loginController");
 const addNewMemberController = require("./controllers/addNewMemberController");
 const groupAdministrationController = require("./controllers/groupAdministrationController");
 const createNewGroupController = require("./controllers/createNewGroupController");
 const startNewProjectController = require("./controllers/startNewProjectController");
-
+*/
 app.get("/", homeController.getHome);
+
+
+/*
 app.get("/settings", settingsController.getSettings);
 app.get("/login", loginController.getLogin);
 app.get("/add-new-member", addNewMemberController.getAddNewMember);
@@ -26,6 +47,34 @@ app.get(
 );
 app.get("/create-new-group", createNewGroupController.getCreateNewGroup);
 app.get("/start-new-project", startNewProjectController.getStartNewProject);
+*/
+
+/* handlebars */
+
+
+app.get('/addNewMembers', (reg, res) => {
+  res.render('addNewMembers');
+});
+
+app.get('/createNewGroup', (reg, res) => {
+  res.render('createNewGroup');
+});
+
+app.get('/index', (reg, res) => {
+  res.render('index');
+});
+
+app.get('/settings', (reg, res) => {
+  res.render('settings');
+});
+
+app.get('/startNewProject', (reg, res) => {
+  res.render('startNewProject');
+});
+
+
+ /* */
+
 
 app.use((req, res, next) => {
   res.status(404).send("Page not found");
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,26 @@
-const path = require("path");
 
+const express = require("express");
+const app = express();
+
+const { engine } = require('express-handlebars');
+const path = require("path");
+app.set('view engine', 'hbs');
+
+
+
+// Set up Handlebars as the template engine
+app.engine('hbs', engine({
+    defaultLayout: 'main',  
+    extname: '.hbs'
+}));
+
+app.get('/', (reg, res) => {
+  res.render('index');
+});
+
+/*
 exports.getHome = (req, res) => {
   res.sendFile(path.join(__dirname, "../views", "index.html"));
 };
+
+*/
