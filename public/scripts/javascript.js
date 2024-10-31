32fd2acd297259ea250dbcfe4a0f1dfa3305c00f
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2024-10-31 09:17:38 +0100

    dark light mode switch js
    missing event connection

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -67,3 +67,33 @@ function toggleAccordion2(element) {
     element.querySelector('.arrow-icon2').style.transform = "rotate(180deg)";
   }
 }
+
+//* Dark light mode switch //
+
+const modeBtn = document.getElementById('mode');
+modeBtn.onchange = (e) => {
+  if (modeBtn.checked === true) {
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.add("dark")
+    window.localStorage.setItem('mode', 'dark');
+  } else {
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+    window.localStorage.setItem('mode', 'light');
+  }
+}
+
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light")
+  document.documentElement.classList.add("dark")
+}
+
+if (mode == 'light') {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark")
+  document.documentElement.classList.add("light")
+}
+
+// Mode end//
