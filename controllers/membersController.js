a668e881743975a71b4ada29e54eaa461ea4729a
BawarHamid <bawar@live.dk>
2024-11-25 21:56:48 +0100

    clean up.

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,10 +1,4 @@
-const { stopStack } = require("../services/portainerService");
-
 exports.getMembers = async (req, res) => {
-  const stackId = 85;
-  const stopResult = await stopStack(stackId);
-  console.log(`Stack ${stackId} stopped successfully:`, stopResult);
-
   res.render("members", {
     title: "Members",
   });
--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -1,9 +1,4 @@
-const { startStack } = require("../services/portainerService");
-
 exports.getSettings = async (req, res) => {
-  const stackId = 85;
-  const startResult = await startStack(stackId);
-  console.log(`Stack ${stackId} started successfully:`, startResult);
   res.render("settings", {
     title: "Settings",
   });
