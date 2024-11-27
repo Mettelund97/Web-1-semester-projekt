3c532e3d0624197bb2d1cc348cc19d41b1f0cbe6
BawarHamid <bawar@live.dk>
2024-11-27 09:49:47 +0100

    stack in db

--- services/portainerService.js
+++ services/portainerService.js
@@ -59,9 +59,7 @@ class PortainerService {
 
   async createStack(projectName, subDomain) {
     try {
-      const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwidXNlcm5hbWUiOiJhYm1tIiwicm9sZSI6Miwic2NvcGUiOiJkZWZhdWx0IiwiZm9yY2VDaGFuZ2VQYXNzd29yZCI6ZmFsc2UsImV4cCI6MTczMjY0MDY5NiwiaWF0IjoxNzMyNjExODk2LCJqdGkiOiI3YzIyNmVjYS02NTZkLTRmNGUtYWE4Mi1hNTkyNTI2MDQ4NmYifQ.Mn82uXwfQa1HHC6VJS4FV3MYHnQ_xQ3ny_stGGhv9k4";
-      // const token = await getConfig("PORTAINERTOKEN");
+      const token = await configModel.getConfig("PORTAINERTOKEN");
       const websiteId = Math.random().toString(36).substring(7);
 
       const stackFileContent = String.raw`version: '3.8'
@@ -200,9 +198,7 @@ services:
 
   async deleteStack(stackId) {
     try {
-      // const token = await getConfig("PORTAINERTOKEN");
-      const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwidXNlcm5hbWUiOiJhYm1tIiwicm9sZSI6Miwic2NvcGUiOiJkZWZhdWx0IiwiZm9yY2VDaGFuZ2VQYXNzd29yZCI6ZmFsc2UsImV4cCI6MTczMjY0MDY5NiwiaWF0IjoxNzMyNjExODk2LCJqdGkiOiI3YzIyNmVjYS02NTZkLTRmNGUtYWE4Mi1hNTkyNTI2MDQ4NmYifQ.Mn82uXwfQa1HHC6VJS4FV3MYHnQ_xQ3ny_stGGhv9k4";
+      const token = await configModel.getConfig("PORTAINERTOKEN");
       const response = await axios.delete(
         `${this.baseUrl}/stacks/${stackId}?endpointId=${this.endpointId}`,
         {
